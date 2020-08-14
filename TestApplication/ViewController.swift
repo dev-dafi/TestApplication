1f81f8dbb94ec95a4102c7889c6949f0939fa216
Daniel Fischer <dev_dafi@icloud.com>
2020-08-14 13:28:29 +0200

    Initial Commit

---
+++ TestApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestApplication
+//
+//  Created by Daniel Fischer on 14.08.20.
+//  Copyright © 2020 Daniel Fischer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
